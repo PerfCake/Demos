aa4e44364eb780c30ae5538f1e5bfec118124cc3
Martin Večeřa <mvecera@redhat.com>
2016-09-01 10:33:15 +0200

    weaver server

--- Weaver/src/main/java/org/perfcake/examples/weaver/Weaver.java
+++ Weaver/src/main/java/org/perfcake/examples/weaver/Weaver.java
@@ -19,6 +19,51 @@
  */
 package org.perfcake.examples.weaver;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+import java.io.File;
+import java.util.concurrent.Executors;
+
 public final class Weaver {
 
+   @Parameter(names = {"-t", "--threads"}, description = "Number of threads, 0 = unlimited")
+   private int threads = 0;
+
+   @Parameter(names = {"-s", "--shuffle"}, description = "Shuffle the workers", arity = 1)
+   private boolean shuffle = false;
+
+   @Parameter(names = {"-c", "--config"}, description = "Configuration file", required = true)
+   private String config = null;
+
+   @Parameter(names = {"--help"}, description = "Prints out command usage")
+   private boolean help = false;
+
+   @Parameter(names = {"-p", "--port"}, description = "Network port to listen on")
+   private int port = 8080;
+
+   @Parameter(names = {"-h", "--host"}, description = "Network host to listen on")
+   private String host = "localhost";
+
+   public static void main(String[] args) {
+      final Weaver weaver = new Weaver();
+      final JCommander jCommander = new JCommander(weaver, args);
+
+      if (weaver.config == null || weaver.help) {
+         jCommander.usage("weaver");
+         System.exit(1);
+      }
+
+      if (!(new File(weaver.config).exists())) {
+         System.out.println("The specified configuration file '" + weaver.config + "' does not exists.");
+         System.exit(2);
+      }
+   }
+
+   private void init() {
+   }
+
+   private void run() {
+
+   }
 }
